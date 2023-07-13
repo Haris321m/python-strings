06335dd11e0bd22de252b86897cfbe7bb5aeba30
Haris321m <139390380+Haris321m@users.noreply.github.com>
2023-07-14 03:48:06 +0500

    Add files via upload

---
+++ String_Manipulation.py
@@ -0,0 +1,4 @@
+print("Day 1 - String manipulation")
+print("Strint Concatenation is done with the " + "Sign")
+print('e.g print("Hello " + " world")')
+print("New line can be created with a backslash and n")
